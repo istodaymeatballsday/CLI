eb35fea8daeb883eade40a58627eea4b36abbde4
anthager <anthage@student.chalmers.se>
2019-04-15 22:24:21 +0200

    try to do it with python and black awk magic. Also it will add support for macos for the date command

--- bash/expressen.sh
+++ bash/expressen.sh
@@ -1,99 +1,99 @@
-#get jq-filename
-jq=$(find . -maxdepth 1 -name "*jq*")
-
-#check if jq is downloaded
-if [ -z $jq ]; then
-	#get os
-	os=$OSTYPE
-
-	echo "Downloading jq..."
-
-	#linux
-	if [[ "$os" == "linux-gnu" ]]; then		
-		wget -q https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-	#mac
-	elif [[ "$os" == "darwin"* ]]; then
-		curl -o jq-osx-amd64 https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64
-	fi
-
-	#get and trim jq-filename
-	jq=$(find . -maxdepth 1 -name "*jq*")
-	sudo chmod +x ${jq:2}
-fi
+# - - - - Description - - - -
+# outputs Chalmers expressen lunch in terminal, highlights 'Köttbullar'
 
 
-#expressen data
-#optional: download jq library for linux/mac and replace $jq with jq
+# - - - - How to install - - - -
+# $ chmod +x ./expressen.sh <# days from today>
+
+
+# - - - - How to run - - - -
+# $ ./expressen.sh <# days from today>
+
+
+#optional: download jq library for linux/mac and replace ./jq-
 get_expressen_data() {
-	expressen_data=$(curl -s $url | $jq '.[] | .startDate, .displayNames[0].dishDisplayName')
+    # I cried when i wrote that awk
+    expressen_data=$(curl -s $url | python -mjson.tool | grep "dishDisplayName" | awk 'NR % 2 == 1' | awk -F '[(\")|\",]' '{ print $4 }' )
 }
 
 #expressen api
 expressen_api_url() {
-	local api='v1/mealprovidingunits/3d519481-1667-4cad-d2a3-08d558129279/dishoccurrences'
-	url='http://carbonateapiprod.azurewebsites.net/api/'$api''
+    # why have api as a variable and not just a long string?
+    # i.e.  http://carbonateapiprod.azurewebsites.net/api/
+    local api='v1/mealprovidingunits/3d519481-1667-4cad-d2a3-08d558129279/dishoccurrences'
+    url='http://carbonateapiprod.azurewebsites.net/api/'$api''
 }
 
-#return index if string contains 'KöTTBULLAR' 
+#return index if string contains 'KöTTBULLAR'
 is_it_meatballs() {
-	local capital="$(echo "$1" | tr a-z A-Z)"
-	index="$(echo "$capital" | grep -b -o 'KöTTBULLAR' | awk 'BEGIN {FS=":"}{print $1}')"
+    local capital="$(echo "$1" | tr a-z A-Z)"
+    index="$(echo "$capital" | grep -b -o 'KöTTBULLAR' | awk 'BEGIN {FS=":"}{print $1}')"
 }
+
 
 #expressen menu
 lunch() {
-#check if input null or digit or negative
-if [ -z $1 ] || ! [[ "$1" =~ ^[0-9]+$ ]] || [ $1 -lt 0 ]; then
-	echo -e "\nInvalid input\n"
-	return 0
-fi
-
-local today=$(date +'%Y-%m-%d')
-
-#number of days from today
-local end_date=$(date -d "$today+$1 days" +'%Y-%m-%d')
-
-#api
-expressen_api_url
-local url=''$url'?startDate='$today'&endDate='$end_date''
-
-#get data
-get_expressen_data "$1"
-local data=$expressen_data
-if [ -z "$data" ]; then
-	echo -e "\nNo data\n"
-	return 0
-fi
-	
-#store data in array
-IFS=$'\n' read -r -a arr -d '' <<< "$data"
-
-local length=${#arr[@]}
-local temp=''
-#data is stored: [date, meat, veg, ...]
-for ((i=0; i<$length; i+=2))
-do
-	#trim citation
-	local date=${arr[i]:1:-1}
-	local food=${arr[$((i+1))]:1:-1}
-
-if [ "$date" != "null" ] && [ "$food" != "null" ]; then
-	if [ "$date" != "$temp" ]; then
-		echo -e "\n\e[1m\e[32m$(date --date "$date" +'%A')\e[0m:"
-		temp=$date
-	fi
-
-	#is it meatballs?
-	is_it_meatballs "$food"
-	index=$index
-	if [[ ! -z "$index" ]]; then
-		echo -e "\e[39m\e[5m${food:$index:10}\e[0m${food:10}"
-	else
-		echo -e "$food"
-	fi
-fi
-done
-echo -e ""
+    #check if input null or digit or negative
+    if [ -z $1 ] || ! [[ "$1" =~ ^[0-9]+$ ]] || [ $1 -lt 0 ]; then
+        echo -e "\nInvalid input\n"
+        return 0
+    fi
+    
+    # since the date command works differently on the different environments this is necessary
+    local today=$(date +'%Y-%m-%d')
+    #linux
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        #number of days from today
+        local end_date=$(date -d "$today+$1 days" +'%Y-%m-%d')
+        #mac
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+        #number of days from today
+        local end_date=$(date -v+$1d +'%Y-%m-%d')
+    fi
+    
+    
+    # api
+    expressen_api_url
+    local url=''$url'?startDate='$today'&endDate='$end_date''
+    
+    #get data
+    get_expressen_data "$1"
+    local data=$expressen_data
+    if [ -z "$data" ]; then
+        echo -e "\nNo data\n"
+        return 0
+    fi
+    echo $data
+    
+    # #store data in array
+    # IFS=$'\n' read -r -a arr -d '' <<< "$data"
+    
+    # local length=${#arr[@]}
+    # local temp=''
+    # #data is stored: [date, meat, veg, ...]
+    # for ((i=0; i<$length; i+=2))
+    # do
+    # 	#trim citation
+    # 	local date=${arr[i]:1:-1}
+    # 	local food=${arr[$((i+1))]:1:-1}
+    
+    # if [ "$date" != "null" ] && [ "$food" != "null" ]; then
+    # 	if [ "$date" != "$temp" ]; then
+    # 		echo -e "\n\e[1m\e[32m$(date --date "$date" +'%A')\e[0m:"
+    # 		temp=$date
+    # 	fi
+    
+    # 	#is it meatballs?
+    # 	is_it_meatballs "$food"
+    # 	index=$index
+    # 	if [[ ! -z "$index" ]]; then
+    # 		echo -e "\e[39m\e[5m${food:$index:10}\e[0m${food:10}"
+    # 	else
+    # 		echo -e "$food"
+    # 	fi
+    # fi
+    # done
+    echo -e ""
 }
 
 #executes function with input $1 as '# of days from today'
